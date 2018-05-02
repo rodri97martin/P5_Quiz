809328afb027155f0436113f9461588b09814695
Rodrigo Martín Martín <Rodri@MacBook-Pro-de-Rodrigo.local>
2018-05-02 16:22:56 +0200

    Quizzes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var Sequelize = require('sequelize');
+var sequelize = require('../models/index')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +11,20 @@ router.get('/', function(req, res, next) {
 
 router.get('/credits', function(req, res, next) {
     res.render('credits', { name: 'Rodrigo Martín Martín' });
+});
+
+router.get('/quizzes', function(req, res, next) {
+    sequelize.models.quiz.findAll()
+        .then(quizzes => {
+            res.render('quizzes', {quizzes});
+        })
+        .catch(Sequelize.ValidationError, error => {
+            console.log('El quiz es erróneo.');
+            error.errors.forEach(({message}) => errorlog(message));
+        })
+        .catch(error => {
+            console.log(error.message);
+        })
 });
 
 module.exports = router;
